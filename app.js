06dc512cb29d0ebd99f53366935d3c1b06a85566
j04quin <joaco2850@gmail.com>
2022-12-22 14:24:50 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+const port = process.env.PORT || 3001;
+app.listen(port, () =>console.log(`Servidor corriendo en el puerto ${port}`));
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
